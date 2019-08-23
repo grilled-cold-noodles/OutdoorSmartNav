141ee02f471abfffc1eb2073d921a34392ac34c1
Xing-lil <836567328@qq.com>
2019-08-23 12:55:22 +0800

    update ip

--- IndoorNav/app/src/main/java/cn/mcf/myapplication/netInf.java
+++ IndoorNav/app/src/main/java/cn/mcf/myapplication/netInf.java
@@ -7,6 +7,7 @@ package cn.mcf.myapplication;
 public class netInf {
     static String ip = "140.143.248.178";
     static String port = "10008";
-    static String ipAli = "47.102.149.164";
+//    static String ipAli = "47.102.149.164"; //fzh
+    static String ipAli = "39.106.58.247";
     static String portAli = "30000";
 }
